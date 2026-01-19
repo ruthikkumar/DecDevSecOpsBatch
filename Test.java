be79a92351c3f53906e5f1ff5d5d5022123b4643
ruthikkumar <ruthikkumar@gmail.com>
2026-01-19 09:57:41 +0000

    Revert "changed age validation wrongly buggy code commit"
    This reverts commit cd2aac32bf6cb16b5e974460d0347a5318e9e425.

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 class Test {
         public static void main(String[] args){
                 int age = 18;
-                if (age == 18){
+                if (age >= 18){
                         System.out.println("Access allowed");
                 }
                    else {
